bffa50460e7ae2b48b63d163329ca3567623d96c
Ashenbark <aisu-uy@outlook.com>
2022-04-18 13:20:21 +0200

    Update 18/04

--- Main.py
+++ Main.py
@@ -88,7 +88,7 @@ if initial != 0:
         np.save("test.npy", history_cat)
 
     except Exception as e:
-        np.save("history.npy", history.history)
+        np.save("history2.npy", history.history)
         print(e)
 
 else:
--- historyprinter.py
+++ historyprinter.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 history = np.load("history.npy", allow_pickle=True)
 
 plt.figure()
-plt.title("Loss")
+plt.title("Loss vs Epochs")
 plt.plot(history.item()['loss'])
 plt.plot(history.item()['val_loss'])
-# plt.legend(['Training Loss', 'Validation Loss'])
+plt.legend(['Training Loss', 'Validation Loss'])
 
 plt.figure()
-plt.title("Accuracy")
+plt.title("Accuracy vs Epochs")
 plt.plot(history.item()['accuracy'])
 plt.plot(history.item()['val_accuracy'])
-# plt.legend(['Training accuracy', 'Validation accuracy'])
+plt.legend(['Training accuracy', 'Validation accuracy'])
 
 plt.show()
